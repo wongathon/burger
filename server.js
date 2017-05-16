d3dd4fdb96de66c91d01281b98bc65b02c3b7f44
Andrew Wong <andrew.wong@andrew-wong-compy.local>
2017-05-15 20:34:26 -0400

    dbs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
