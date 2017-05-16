d538b1cad469fad16615a140f8a5650515d576d6
Andrew Wong <andrew.wong@andrew-wong-compy.local>
2017-05-15 20:05:31 -0400

    heroku db

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  console.log(process.env.JAWSDB_URL);
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
